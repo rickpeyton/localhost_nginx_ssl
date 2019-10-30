f81d46b17eb6aee63cf2e31f5be8fe98b26b0d4e
Joél Morales <joel.morales@daveramsey.com>
2019-10-30 15:15:03 -0500

    allow spaces in path

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run -it --rm -p 443:443 $(basename `pwd`)
+echo "Started on port 443"
+docker run -it --rm -p 443:443 $(basename "$(pwd)")
+# exiting requires Ctrl-C, so skip a line to print a nice message
+echo
+echo "All done."
